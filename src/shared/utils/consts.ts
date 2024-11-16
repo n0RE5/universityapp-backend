f0c5e5e1b4cb0bfbcf38230b60d51ab0106785ee
MH <mail@mail.com>
2024-11-16 12:58:52 +0300

    [!] Fixed bot token value

--- src/shared/utils/consts.ts
+++ src/shared/utils/consts.ts
@@ -2,5 +2,5 @@ require('dotenv').config()
 
 export const BOT_TOKEN = process.env.BOT_TOKEN as string
 export const TELEGRAM_BOT_API_URL = process.env.TELEGRAM_ENV === 'test'
-                                        ? "https://api.telegram.org/bot${BOT_TOKEN}/test/"
-                                        : "https://api.telegram.org/bot${BOT_TOKEN}/"
+                                        ? `https://api.telegram.org/bot${BOT_TOKEN}/test/`
+                                        : `https://api.telegram.org/bot${BOT_TOKEN}/`
