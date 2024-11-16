388197285970a1bd5e95950100f1cee3741b504f
MH <mail@mail.com>
2024-11-16 11:54:41 +0300

    [~] Hosting fix

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,24 @@ async function bootstrap() {
       transform: true, 
       whitelist: true 
   }));
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    res.setHeader("Access-Control-Max-Age", 7200);
+    next();
+  })
   app.setGlobalPrefix('/api')
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
